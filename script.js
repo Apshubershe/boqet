fd16c458f8518ee00abcd4ec28a8858710f63ce7
Apshubershe <kanalyoukap@gmail.com>
2026-02-05 13:41:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-const bouquet = document.getElementById("bouquet");
 
-bouquet.addEventListener("click", () => {
-  const flower = document.createElement("div");
-  flower.classList.add("flower");
-
-  // случайный цвет
-  const colors = ["#ff6f91", "#ffc75f", "#f9a1bc", "#cdb4db"];
-  flower.style.background = colors[Math.floor(Math.random() * colors.length)];
-
-  bouquet.appendChild(flower);
-});
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+  };
